c0ff657c2a37f83f362821218b782732cee444d2
James <32380918+houlahaj@users.noreply.github.com>
2020-03-14 22:43:55 +0100

    feat: use real time.Time for datetime (#2)

--- pricing_types.go
+++ pricing_types.go
@@ -1,14 +1,41 @@
 package goanda
 
-import "github.com/shopspring/decimal"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 // ClientPrice represents an account-specific price of an instrument.
 type ClientPrice struct {
 	Instrument string
-	Time       string
 	Tradeable  bool
 	Bids       []PriceBucket
 	Asks       []PriceBucket
+	Time       time.Time
+}
+
+func (p *ClientPrice) UnmarshalJSON(b []byte) (err error) {
+	type _ClientPrice ClientPrice
+
+	rawPrice := struct {
+		_ClientPrice
+
+		Time string
+	}{}
+
+	if err = json.Unmarshal(b, &rawPrice); err != nil {
+		return
+	}
+
+	*p = ClientPrice(rawPrice._ClientPrice)
+
+	if p.Time, err = time.Parse(time.RFC3339Nano, rawPrice.Time); err != nil {
+		return
+	}
+
+	return
 }
 
 // PriceBucket represents a price available for an amount of liquidity.
--- trade_types.go
+++ trade_types.go
@@ -1,6 +1,9 @@
 package goanda
 
 import (
+	"encoding/json"
+	"time"
+
 	"github.com/shopspring/decimal"
 )
 
@@ -17,9 +20,38 @@ type Trade struct {
 	UnrealizedPL          decimal.Decimal
 	MarginUsed            decimal.Decimal
 	AverageClosePrice     decimal.Decimal
+	OpenTime              time.Time
+	CloseTime             time.Time
 	ClosingTransactionIDs []string
-	OpenTime              string // TODO: should be time.Time, parsed according to RFC3339
-	CloseTime             string // TODO: should be time.Time, parsed according to RFC3339
+}
+
+func (t *Trade) UnmarshalJSON(b []byte) (err error) {
+	type _Trade Trade
+
+	rawTrade := struct {
+		_Trade
+
+		OpenTime  string
+		CloseTime string
+	}{}
+
+	if err = json.Unmarshal(b, &rawTrade); err != nil {
+		return
+	}
+
+	*t = Trade(rawTrade._Trade)
+
+	if t.OpenTime, err = time.Parse(time.RFC3339Nano, rawTrade.OpenTime); err != nil {
+		return
+	}
+
+	if rawTrade.State == Closed {
+		if t.CloseTime, err = time.Parse(time.RFC3339Nano, rawTrade.CloseTime); err != nil {
+			return
+		}
+	}
+
+	return
 }
 
 // TradeState represents the current state of a trade.
